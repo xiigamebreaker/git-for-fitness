406470ff7709eb5351305e056e448bf79dd98cc4
Noah Amman <noahamman@yahoo.com>
2026-03-08 18:16:10 -0500

    Worked on some objects for fun.

---
+++ script.js
@@ -0,0 +1,69 @@
+// rabbit object with a method that uses 'this' to access its color property
+const rabbit = {
+    color : "white",
+    checkWatch() {
+        console.log(`A ${this.color} rabbit checks his watch and exclaims I'm late!`);
+    }
+};
+rabbit.checkWatch();
+
+// spaceship object with a method that uses 'this' to access its name property
+const spaceship = {
+    name: "Apolla 11",
+    launch() {
+        return `You have launched the spaceship ${this.name}!`;
+    }
+}
+console.log(spaceship.launch());
+
+// shopping cart object with methods to add and remove items, using 'this' to access the contents array
+const shoppingCart = {
+    contents : [],
+    addItem(item) {
+        this.contents.push(item);
+    },
+
+    removeItem(item) {
+        let indexNum = this.contents.indexOf(item)
+        if (indexNum > -1) {
+            this.contents.splice(indexNum, 1)
+            console.log("Success!")
+        } else {
+            console.log("That item does not exist.")
+        }
+    }
+};
+shoppingCart.addItem("laptop"); 
+shoppingCart.addItem("mac"); 
+for (items of shoppingCart.contents) {
+    console.log(items)
+} 
+
+
+shoppingCart.removeItem("laptop"); 
+shoppingCart.removeItem("phone"); 
+console.log(items); 
+
+// lever object with a method that uses 'this' to access its stamp property
+const lever = {
+    stamp: "ACME",
+    pull () {
+        console.log(`An anvil stamped ${this.stamp} drops on your head.`)
+    }
+}
+lever.pull();
+
+// deadPool object with a method that uses 'this' to access its properties
+const deadPool = {
+    name: "Wade Wilson",
+    weapon : "Katana",
+    health: "Immortal",
+    suit : {
+        color: "red and black"
+    },
+    
+    fight() {
+        return `${this.name} fights with a ${this.weapon} and since he is ${this.health}, then he can never die, but his ${this.suit.color} suit always gets damaged.`
+    }
+}
+console.log(deadPool.fight());
